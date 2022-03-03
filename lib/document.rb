e05ce4992ddf6a7cb195871e7d2ca8ffc44dc5c3
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-03 18:32:42 +0100

    Add sections text to document

--- lib/document.rb
+++ lib/document.rb
@@ -1,32 +1,43 @@
-require "erb"
+require 'erb'
 
 # build data class
 class Listings
-  PRODUCT = { :name => "A T&C Document",
-              :desc => "This document is made of plaintext.",
+  PRODUCT = { :name => 'A T&C Document',
+              :desc => 'This document is made of plaintext.',
               :uniform_word => 'Is made of',
-              :clause_3 => 'And dies',
-              :clause_4 => 'The white horse is white',
-              :footer => 'Your legals.'
-            #   :SECTION-1 => 'The white horse is white',
+              #   :clause_3 => 'And dies.',
+              #   :clause_4 => 'The white horse is white.',
+              #   :footer => 'Your legals.',
+              #   :SECTION-1 => []
+              :clauses => [
+                { 'id': 1, 'text': 'The quick brown fox.' },
+                { 'id': 2, 'text': 'Jumps over the lazy dog.' },
+                { 'id': 3, 'text': 'And dies.' },
+                { 'id': 4, 'text': 'The white horse is white.' }
+              ],
+              :sections => [
+                { "id": 1, "clauses_ids": [1, 2] }
+              ]
              }
+  #   :SECTION-1 => 'The white horse is white',
 
-  attr_reader :product, :clause
+  attr_reader :product
 
-  def initialize( product = "", clause = "" )
+  def initialize(product = '')
     @product = product
-    @clause = clause
   end
 
   def build
     b = binding
     # create and run templates, filling member data variables
-    ERB.new(<<-'END_PRODUCT'.gsub(/^\s+/, ""), 0, "", "@product").result b
+    ERB.new(<<-'END_PRODUCT'.gsub(/^\s+/, ''), 0, '', '@product').result b
       <%= PRODUCT[:name] %>
       <%= PRODUCT[:desc] %>
-      <%= PRODUCT[:uniform_word] %>  <%= PRODUCT[:clause_3] %>
-      <%= PRODUCT[:uniform_word] %>  <%= PRODUCT[:clause_4] %>
-      
+      <%= PRODUCT[:uniform_word] %>  <%= PRODUCT[:clauses][2][:text] %>
+      <%= PRODUCT[:uniform_word] %>  <%= PRODUCT[:clauses][3][:text] %>
+      <%= PRODUCT[:uniform_word] %>  <%= PRODUCT[:sections][0][:clauses_ids] = 
+      PRODUCT[:clauses][0][:text], PRODUCT[:clauses][1][:text] %>
+
       <%= PRODUCT[:footer] %>
     END_PRODUCT
   end
@@ -36,4 +47,4 @@ end
 listings = Listings.new
 listings.build
 
-puts listings.product + "\n" + listings.clause
+puts listings.product
